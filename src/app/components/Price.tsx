af7f98ba071875a135fb07d12f78cbf9903f36c8
EN CHI YANG <wooclmd@gmail.com>
2023-08-21 14:29:05 +1200

    Single product page updated

---
+++ src/app/components/Price.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+type ProductOption = {
+  title: string;
+  additionalPrice: number;
+};
+
+type Props = {
+  price: number;
+  id: number;
+  option1?: ProductOption[];
+  option2?: ProductOption[];
+};
+
+const Price = ({ price, option1, option2, id }: Props) => {
+  //Calculate price
+  const [total, setTotal] = useState(price);
+  const [quantity, setQuantity] = useState(1);
+  const [selected1, setSelected1] = useState(0);
+  const [selected2, setSelected2] = useState(0);
+  useEffect(() => {
+    let totalPrice = price;
+    if (option1 && option1[selected1]) {
+      totalPrice += option1[selected1].additionalPrice;
+    }
+    if (option2 && option2[selected2]) {
+      totalPrice += option2[selected2].additionalPrice;
+    }
+    setTotal(totalPrice * quantity);
+  }, [quantity, selected1, selected2, option1, option2, price]);
+
+  return (
+    <div className="flex flex-col gap-3 justify-around">
+      <p className="font-bold md:text-xl">$ {total}</p>
+      {/* Option container */}
+      {option1 && (
+        <div className="flex items-center gap-3">
+          {option1.map((option, index) => (
+            <button
+              key={id}
+              className="p-1 ring-1 ring-[var(--darker-green)] rounded-md min-w-[6rem]"
+              style={{
+                backgroundColor:
+                  selected1 === index ? "var(--darker-green)" : "white",
+                color: selected1 === index ? "white" : "var(--darker-green)",
+              }}
+              onClick={() => setSelected1(index)}
+            >
+              {option.title}
+            </button>
+          ))}
+        </div>
+      )}
+      {option2 && (
+        <div className="flex items-center gap-3">
+          {option2.map((option, index) => (
+            <button
+              className="p-1 ring-1 ring-[var(--darker-green)] rounded-md min-w-[6rem]"
+              key={id}
+              style={{
+                backgroundColor:
+                  selected2 === index ? "var(--darker-green)" : "white",
+                color: selected2 === index ? "white" : "var(--darker-green)",
+              }}
+              onClick={() => setSelected2(index)}
+            >
+              {option.title}
+            </button>
+          ))}
+        </div>
+      )}
+      {/* Cart container */}
+      <div className="flex gap-2 items-center">
+        {/* Quantity */}
+        <div className="p-1 flex justify-between items-center w-full ring-1 ring-[var(--darker-green)]">
+          <span>Quantity</span>
+          <div className="flex gap-4 items-center">
+            <button
+              onClick={() => setQuantity((prev) => (prev > 1 ? prev - 1 : 1))}
+            >
+              {"<"}
+            </button>
+            <span>{quantity}</span>
+            <button onClick={() => setQuantity((prev) => prev + 1)}>
+              {">"}
+            </button>
+          </div>
+        </div>
+        <div>
+          <button className="p-1 w-40 ring-1 ring-[var(--darker-green)] bg-[var(--darker-green)] text-white">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Price;
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Image from "next/image";
+import { singleProduct } from "@/data";
+import Price from "@/app/components/Price";
 
 const SingleProductPage = () => {
-  return <div>SingleProductPage</div>;
+  return (
+    <div className="p-2 md:p-4 flex flex-col gap-2 h-screen md:h-[60vh] md:flex-row md:gap-4">
+      {/* Image container */}
+      {singleProduct.img && (
+        <div className="relative w-full h-1/2 md:flex-1 md:h-full">
+          <Image
+            src={singleProduct.img}
+            alt="Product"
+            fill
+            className="object-cover"
+          />
+        </div>
+      )}
+
+      {/* Text container */}
+      <div className="text-gray-500 flex flex-col gap-2 md:flex-1 md:justify-center md:gap-4">
+        <h1 className="font-bold text-lg text-[var(--darker-green)] md:text-2xl">
+          {singleProduct.title}
+        </h1>
+        <p className="text-justify">{singleProduct.desc}</p>
+        <Price
+          price={singleProduct.price}
+          id={singleProduct.id}
+          option1={singleProduct.option1}
+          option2={singleProduct.option2}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SingleProductPage;
