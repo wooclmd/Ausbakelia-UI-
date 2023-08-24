e1994d24e68166accf17612514534ce9239c6109
EN CHI YANG <wooclmd@gmail.com>
2023-08-24 16:00:25 +1200

    hotfix

--- src/app/components/Offer.tsx
+++ src/app/components/Offer.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -18,9 +20,9 @@ const Offer = () => {
       <div className="p-6 flex-1 text-white flex flex-col gap-5 items-center justify-center">
         <h1 className="text-2xl font-bold">Moon Festival Coffee Mix Box</h1>
         <p style={{ textAlign: "justify" }}>
-          Ausbakelia&apos;s coffee undergoes a journey from bean selection,
-          roasting, to grinding – all meticulously curated by our in-house
-          baristas, giving it a unique coffee essence.
+          Ausbakelia's coffee undergoes a journey from bean selection, roasting,
+          to grinding – all meticulously curated by our in-house baristas,
+          giving it a unique coffee essence.
         </p>
         <Link
           href={"/menu"}
